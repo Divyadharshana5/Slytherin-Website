706c0e24a9992ec5b710caa1ae53c249142deedf
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-02-28 20:13:17 +0530

    upated the achievement and teams page

--- src/App.jsx
+++ src/App.jsx
@@ -2,41 +2,28 @@ import React from "react";
 import Contact from "./components/Contact/Contact";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
-import About from"./components/About/About"
+import About from "./components/About/About";
+import Achievement from "./components/Achievement/Achievement";
 import "./App.css";
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      
-      
-      <section id="hero" style={{  }}>
-      </section>
-      <Hero/>
-        
 
-        
-      <section id="about" style={{  }}>
-        </section>
-      <About/>
-      <section id="teams" style={{ }}>
-        
-      </section>
-      <section id="achievements" style={{  }}>
-      
-      </section>
-      <section id="projects" style={{  }}>
-       
-      </section>
-      <section id="contact" style={{  }}>
-       
-        <Contact/>
+      <section id="hero" style={{}}></section>
+      <Hero />
+
+      <section id="about" style={{}}></section>
+      <About />
+      <section id="teams" style={{}}></section>
+      <section id="achievements" style={{}}></section>
+      <section id="projects" style={{}}></section>
+      <section id="contact" style={{}}>
+        <Contact />
       </section>
     </div>
   );
 };
 
 export default App;
-
-
