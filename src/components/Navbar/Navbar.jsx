b4107d4d713c09e91d2b807f45ad50c513365931
janani k <145999085+Jananikaavya@users.noreply.github.com>
2025-06-15 18:20:18 +0530

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,27 +7,29 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="container">
-        <h1 className="logo">Slytherin</h1>
+        <a href="/" className="logo-link">
+          <img src="/s1-Photoroom.png" alt="Slytherin Logo"   className="logo-image" />
+        </a>
+
         <ul className={menuOpen ? "nav-links open" : "nav-links"}>
-          <li>
-            <a href="#home">Home</a>
-          </li>
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#teams">Teams</a>
-          </li>
-          <li>
-            <a href="#achievements">Achievements</a>
-          </li>
-          <li>
-            <a href="#projects">Projects</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/teams">Teams</a></li>
+          <li><a href="/contact">Contact</a></li>
         </ul>
+
+        <div className="social-icons">
+          <a href="https://github.com/Divyadharshana5/Slytherin-Website" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-github"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/slytherin-private-limited-solution-a40516353" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-linkedin"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/slytherin-private-limited-solution-a40516353" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-youtube"></i>
+          </a>
+        </div>
+
         <div className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
           ☰
         </div>
@@ -37,3 +39,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
