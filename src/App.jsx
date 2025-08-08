d7fe21383231f86c1307b1a8da1938b30f954ac0
janani k <145999085+Jananikaavya@users.noreply.github.com>
2025-08-08 12:23:58 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -4,44 +4,32 @@ import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import Teams from "./components/Teams/Teams";
+import Services from "./components/Services/Services";
 import Contact from "./components/Contact/Contact";
+import Projects from "./components/Projects/Projects";
 
-import TeamMember from "./components/Teams/TeamMember";
-import bgVideo from "./assets/bgvideo.mp4";
-import "./App.css";
+import './App.css'
 
-const App = () => {
+const App=()=>{
   return (
     <>
       <Router>
-        {/* ✅ Background Video */}
-        <video
-          className="video-background"
-          autoPlay
-          muted
-          loop
-          playsInline
-        >
-          <source src={bgVideo} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-
         <Navbar />
-
         <div className="main-content">
           <Routes>
             <Route path="/" element={<Hero />} />
             <Route path="/about" element={<About />} />
             <Route path="/teams" element={<Teams />} />
-            <Route path="/member/:id" element={<TeamMember />} />
-            
             <Route path="/contact" element={<Contact />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/projects" element={<Projects />} />
+            
           </Routes>
         </div>
+       
       </Router>
     </>
   );
 };
 
 export default App;
-
