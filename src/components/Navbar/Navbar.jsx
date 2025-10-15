6edba398c0e96b81cae475dcb411639dd0ae1ab9
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-10-15 09:54:50 +0530

    Refactor Navbar component for improved readability by adjusting formatting and ensuring consistent styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,34 +8,58 @@ const Navbar = () => {
     <nav className="navbar">
       <div className="container">
         <a href="/" className="logo-link">
-          <img src="/s1-Photoroom.png" alt="Slytherin Logo"   className="logo-image" />
+          <img
+            src="/s1-Photoroom.png"
+            alt="Slytherin Logo"
+            className="logo-image"
+          />
         </a>
 
         <ul className={menuOpen ? "nav-links open" : "nav-links"}>
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/teams">Teams</a></li>
-          <li><a href="/services">Services</a></li>
-          <li><a href="/contact">Contact</a></li>
-          
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/teams">Teams</a>
+          </li>
+          <li>
+            <a href="/services">Services</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
         </ul>
 
         <div className="social-icons">
-          <a href="https://github.com/Divyadharshana5/Slytherin-Website" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://github.com/Divyadharshana5/Slytherin-Website"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <i className="fab fa-github"></i>
           </a>
-          <a href="https://www.linkedin.com/in/slytherin-private-limited-solution-a40516353" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://www.linkedin.com/in/slytherin-private-limited-solution-a40516353"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <i className="fab fa-linkedin"></i>
           </a>
-          <a href="https://www.linkedin.com/in/slytherin-private-limited-solution-a40516353" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://www.linkedin.com/in/slytherin-private-limited-solution-a40516353"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <i className="fab fa-youtube"></i>
           </a>
         </div>
 
-       <div className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
-  <i className={menuOpen ? "fas fa-times" : "fas fa-bars"}></i>
-</div>
-
+        <div className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
+          <i className={menuOpen ? "fas fa-times" : "fas fa-bars"}></i>
+        </div>
       </div>
     </nav>
   );
