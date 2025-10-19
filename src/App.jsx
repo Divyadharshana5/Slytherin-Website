3ff9209e63799e810c72001f0094d545b16b7eb8
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-10-19 21:53:50 +0530

    Update Router component to include future features for improved routing capabilities

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import "./App.css";
 const App = () => {
   return (
     <>
-      <Router>
+      <Router future={{ v7_relativeSplatPath: true, v7_startTransition: true }}>
         <Navbar />
         <div className="main-content">
           <Routes>
